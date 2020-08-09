5a5e19eec7725863431019b02fdce6ec721ecc80
nil-is-lin <nil_lilin@163.com>
2020-08-09 17:09:50 +0800

    添加并测试write_surf, read_surf

--- src/RoughSurface.jl
+++ src/RoughSurface.jl
@@ -1,5 +1,60 @@
 module RoughSurface
 
 # Write your package code here.
+using DelimitedFiles, JLD2, FileIO
 
-end
+
+```@meta
+CurrentModule = RoughSurface
+```
+
+export write_surf, read_surf
+
+"""
+    write_surf()
+
+将表面数据写入文件, 默认txt文件的数据用制表符分割, csv文件的数据用``,''分割,
+jld2则为直接保存二进制文件
+"""
+function write_surf(file_path, surf)
+    # 利用splitext来得到文件类型
+    file_ext = splitext(file_path)[2]
+    if file_ext == ".txt"
+        open(file_path, "w") do io
+            writedlm(io, surf)
+        end # do
+    elseif file_ext == ".csv"
+        open(file_path, "w") do io
+            writedlm(io, surf, ',')
+        end # do
+    elseif file_ext == ".jld2"
+        save(file_path, "surf", surf)
+    else
+        @warn("未知的文件类型, 保存为jld2格式")
+        save(file_path*".jld2", "surf", surf)
+    end
+
+end # function
+
+"""
+    read_surf()
+
+从文件读取表面数据, 与写入数据保持一致, 默认txt文件的数据用制表符分割, csv文件的数据用``,''分割,
+jld2则为直接保存二进制文件.
+"""
+function read_surf(file_path)
+    file_ext = splitext(file_path)[2]
+    if file_ext == ".txt"
+        surf = readdlm(file_path)
+    elseif file_ext == ".csv"
+        surf = readdlm(file_path, ',')
+    elseif file_ext == ".jld2"
+        surf = load(file_path,"surf")
+    else
+        @warn("未知的文件类型, 尝试jld2格式")
+        surf = load(file_path*".jld2","surf")
+    end
+    return surf
+end # function
+
+end  # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
-using RoughSurface
+using RoughSurface, Documenter
 using Test
 
 @testset "RoughSurface.jl" begin
     # Write your tests here.
+    file_path = ["test.txt", "test.csv", "test.jld2"]
+    surf = collect(reshape(1:3*3,3,3))
+    @testset for i = 1:length(file_path)
+        write_surf(file_path[i], surf)
+        surf_read = read_surf(file_path[i])
+        @test surf_read == surf
+        rm(file_path[i])
+    end # for
+    # 文档
+    doctest(RoughSurface)
 end
